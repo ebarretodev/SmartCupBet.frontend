e89ef5af8c583ca94bb2e923ba06982383e50c20
Eliabel Barreto <eliabeljunior@gmail.com>
2022-11-22 21:38:24 -0300

    last change

--- src/Pages/BetPlace/components/FormBet.jsx
+++ src/Pages/BetPlace/components/FormBet.jsx
@@ -1,59 +1,37 @@
-import React from 'react';
-import { Button, Divider, Form, Input, Modal, Radio, Typography } from 'antd';
+import React from "react"
+import { Button, Divider, Form, Input, Modal, Radio, Typography } from "antd"
 
-const { Text } = Typography;
+const { Text } = Typography
 
 const FormBet = (props) => {
-	const [form] = Form.useForm();
+    const [form] = Form.useForm()
 
-	const handleSubmit = () => {
-		form.validateFields().then((values) => {
-			form.resetFields();
-			props.setVisible(false);
-		});
-	};
+    const handleSubmit = () => {
+        form.validateFields().then((values) => {
+            form.resetFields()
+            props.setVisible(false)
+        })
+    }
 
-	const handleCancel = () => {
-		form.resetFields();
-		props.setVisible(false);
-	};
+    const handleCancel = () => {
+        form.resetFields()
+        props.setVisible(false)
+    }
 
-	return (
-		<Modal
-			visible={props.visible}
-			title={`Match n# ${props.record.match}`}
-			okText='Submit'
-			cancelText='Cancel'
-			onCancel={handleCancel}
-			onOk={handleSubmit}
-		>
-			<Text>
-				{props.record.match}
-			</Text>
-			<Divider />
-			<Form form={form} layout='vertical' name='form_in_modal'>
-				<Form.Item
-					name='design'
-					label='What is your opinion about the CarIOTA design?'
-					rules={[
-						{
-							required: true,
-							message: 'Please select one option!',
-						},
-					]}
-				>
-					<Radio.Group>
-						<Radio value='Good enough'>Good enough</Radio>
-						<Radio value='Very good'>Very good</Radio>
-						<Radio value='Need improvements'>
-							Need improvements
-						</Radio>
-					</Radio.Group>
-				</Form.Item>
-				
-			</Form>
-		</Modal>
-	);
-};
+    return (
+        <Modal
+            visible={props.visible}
+            title={`Match n# ${props.record.match}`}
+            okText="Submit"
+            cancelText="Cancel"
+            onCancel={handleCancel}
+            onOk={handleSubmit}
+        >
+            <Text> This is the information to make a bet for match n# {props.record.match}.</Text>
+            <Divider />
+            <Form form={form} layout="vertical" name="form_in_modal"></Form>
+        </Modal>
+    )
+}
 
-export default FormBet;
+export default FormBet
